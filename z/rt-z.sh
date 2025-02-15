10adcf353932e19768b52e033e1c30a4b7d31d6f
Prianik <prianikov@gmail.com>
2025-02-15 13:56:56 +0300

    Add files via upload

--- z/rt-z.sh
+++ z/rt-z.sh
@@ -2,12 +2,12 @@
 
 #https://github.com/Prianik/myVPN/raw/refs/heads/main/z/rt-z.sh
 #https://github.com/remittor/zapret-openwrt/releases
-echo ".......Updating OPKG and updating installed packages"
+echo "-----------------------------------------.Updating OPKG and updating installed packages-----------------------------------------"
 opkg update 
 opkg install ca-certificates wget-ssl
 opkg list-upgradable | cut -f 1 -d ' ' | xargs -r opkg upgrade
 
-echo  "Identifying the latest version ZAPRET"
+echo  "-----------------------------------------Identifying the latest version ZAPRET-----------------------------------------"
 rm *
 if [ $# -eq 0 ]; then
     wget  https://github.com/Prianik/myVPN/raw/refs/heads/main/z/zver.txt
@@ -23,25 +23,25 @@ echo ""
 echo "---------------------------------------------------------------------------------------------------------------"
 echo ""
 echo ""
-echo ".......Installed ZAPRET version"${zver}
+echo "-----------------------------------------Installed ZAPRET version----------------------------------------------"${zver}
 wget  https://github.com/Prianik/myVPN/raw/refs/heads/main/z/luci-app-zapret_${zver}_all.ipk
 wget  https://github.com/Prianik/myVPN/raw/refs/heads/main/z/zapret_${zver}_mipsel_24kc.ipk
 
 opkg install zapret_${zver}_mipsel_24kc.ipk
 opkg install luci-app-zapret_${zver}_all.ipk
 
-echo ".......ADD dns instagram"
+echo "-----------------------------------------ADD dns instagram-----------------------------------------"
 wget https://raw.githubusercontent.com/Prianik/myVPN/refs/heads/main/dns.txt
 cat dns.txt >> /opt/zapret/ipset/zapret-hosts-user.txt
 service zapret restart
 
-echo ".......Installed https-dns-proxy"
+echo "-----------------------------------------Installed https-dns-proxy-----------------------------------------"
 opkg install https-dns-proxy
 opkg install luci-app-https-dns-proxy
 service rpcd restart
 
 #
-echo ".......SET Timezone and time"
+echo "-----------------------------------------.SET Timezone and time-----------------------------------------"
 uci set system.@system[0].zonename='Europe/Moscow'
 uci set system.@system[0].timezone='MSK-3'
 uci commit system
@@ -88,7 +88,7 @@ uci set wireless.radio1.disabled=0
 uci commit wireless
 wifi
 
-echo ".......SET IP LAN 172.16.1.1"
+echo "-----------------------------------------SET IP LAN 172.16.1.1-----------------------------------------"
 echo "uci set network.lan.ipaddr='172.16.1.1' && uci commit network && /etc/init.d/network restart"
 uci set network.lan.ipaddr='172.16.1.1'
 uci commit network
