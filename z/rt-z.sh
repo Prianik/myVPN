ad0e015a9ded99751d0a73d4c3c0b16810dca02e
Prianik <prianikov@gmail.com>
2025-02-15 13:53:30 +0300

    Add files via upload

--- z/rt-z.sh
+++ z/rt-z.sh
@@ -1,17 +1,32 @@
 #!/bin/sh
 
-###!/usr/bin/env bash
-####!/usr/bin/env sh
-zver=70.20250213
-
+#https://github.com/Prianik/myVPN/raw/refs/heads/main/z/rt-z.sh
+#https://github.com/remittor/zapret-openwrt/releases
 echo ".......Updating OPKG and updating installed packages"
 opkg update 
 opkg install ca-certificates wget-ssl
 opkg list-upgradable | cut -f 1 -d ' ' | xargs -r opkg upgrade
 
-echo ".......Installed ZAPRET vers "${zver}
+echo  "Identifying the latest version ZAPRET"
+rm *
+if [ $# -eq 0 ]; then
+    wget  https://github.com/Prianik/myVPN/raw/refs/heads/main/z/zver.txt
+    zver=$(cat zver.txt)
+    echo  "Version ZAPRET "${zver}
+else
+    zver=$1
+    echo  "Version ZAPRET "${zver}
+fi
+
+echo ""
+echo ""
+echo "---------------------------------------------------------------------------------------------------------------"
+echo ""
+echo ""
+echo ".......Installed ZAPRET version"${zver}
 wget  https://github.com/Prianik/myVPN/raw/refs/heads/main/z/luci-app-zapret_${zver}_all.ipk
 wget  https://github.com/Prianik/myVPN/raw/refs/heads/main/z/zapret_${zver}_mipsel_24kc.ipk
+
 opkg install zapret_${zver}_mipsel_24kc.ipk
 opkg install luci-app-zapret_${zver}_all.ipk
 
@@ -24,12 +39,14 @@ echo ".......Installed https-dns-proxy"
 opkg install https-dns-proxy
 opkg install luci-app-https-dns-proxy
 service rpcd restart
+
 #
 echo ".......SET Timezone and time"
 uci set system.@system[0].zonename='Europe/Moscow'
 uci set system.@system[0].timezone='MSK-3'
 uci commit system
 /etc/init.d/sysntpd restart
+
 
 echo ".......SET WiFi"
 if [ $# -eq 3 ]; then
@@ -43,6 +60,7 @@ else
     read -p "Enter NameSSID WiFi2.4: " NameSSID1
     read -p "Enter WiFi password: " WiFiKey
 fi
+
 
 # set wifi managed (AP) mode
 uci set wireless.@wifi-iface[0].device=radio0
@@ -77,3 +95,8 @@ uci commit network
 echo ".......Local network IP address changed to 172.16.1.1 !!!!!!!!!!"
 /etc/init.d/network restart
 rm *
+
+
+#смещениее в bin
+#000600E0
+#4yas8rad
