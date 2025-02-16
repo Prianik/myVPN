475645710ad49e498009de064e1bc72877a1ab99
Prianik <prianikov@gmail.com>
2025-02-16 16:32:23 +0300

    Add files via upload

--- z/update.sh
+++ z/update.sh
@@ -1,29 +1,45 @@
 #!/bin/sh
 
+#echo ""
+#echo ".......Updating OPKG ......."
+#echo ""
+#opkg update 
+#echo ""
+#echo ".......Updating installed packages......."
+#echo ""
+#opkg list-upgradable | cut -f 1 -d ' ' | xargs -r opkg upgrade
+
+a1=$(opkg list-installed | grep app-zapret |awk '{print $3}')
 echo ""
-echo ".......Updating OPKG ......."
+echo  echo ".......Installed version-${a1} ......."
 echo ""
-opkg update 
-echo ""
-echo ".......Updating installed packages......."
-echo ""
-opkg list-upgradable | cut -f 1 -d ' ' | xargs -r opkg upgrade
 
 echo ""
 echo  ".......Identifying the latest version ZAPRET......."
 wget  https://github.com/Prianik/myVPN/raw/refs/heads/main/z/zver.txt
 zver=$(cat zver.txt)
+echo ""
+echo  echo ".......Version update ${zver} ......."
+echo ""
 rm zver.txt
-echo  "********Version ZAPRET ******"${zver}
-echo ""
-echo ""
 
-echo ".......Installed or Update ZAPRET version-${zver} ......."
-wget  https://github.com/Prianik/myVPN/raw/refs/heads/main/z/luci-app-zapret_${zver}_all.ipk
-wget  https://github.com/Prianik/myVPN/raw/refs/heads/main/z/zapret_${zver}_mipsel_24kc.ipk
+if [ $a1 -gt ${zver} ]; then
+    echo ""
+    echo ".......Installed or Update ZAPRET version-${zver} ......."
+    echo ""
+    wget  https://github.com/Prianik/myVPN/raw/refs/heads/main/z/luci-app-zapret_${zver}_all.ipk
+    wget  https://github.com/Prianik/myVPN/raw/refs/heads/main/z/zapret_${zver}_mipsel_24kc.ipk
+    opkg install zapret_${zver}_mipsel_24kc.ipk
+    opkg install luci-app-zapret_${zver}_all.ipk
+    rm zapret_${zver}_mipsel_24kc.ipk
+    rm luci-app-zapret_${zver}_all.ipk
+    service zapret restart
+else
+    echo ""
+    echo  "......no new versions found...... "
+    echo ""
+fi
 
-opkg install zapret_${zver}_mipsel_24kc.ipk
-opkg install luci-app-zapret_${zver}_all.ipk
-rm zapret_${zver}_mipsel_24kc.ipk
-rm luci-app-zapret_${zver}_all.ipk
-service zapret restart
+
+
+
